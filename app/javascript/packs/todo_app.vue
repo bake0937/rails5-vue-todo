d272e59f8b3ce59116ed8ea3ee10413e7e035af1
bake0937 <sadayasu.info@gmail.com>
2017-07-30 15:55:59 +0900

    REST APIの手前まで実装

---
+++ app/javascript/packs/todo_app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <input v-model="todo_input">
+    <button @click="clicked">Todo追加</button>
+    <ul>
+      <todo v-for="item in todos" v-bind:todo="item"></todo>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todo from './todo.vue'
+export default {
+  data: function () {
+    return {
+      message: 'Todoアプリ',
+      todo_input: "",
+      todos: [
+        { title: 'TODOアイテム1' },
+        { title: 'TODOアイテム2' },
+      ],
+    }
+  },
+  methods: {
+    clicked: function() {
+      console.log("ボタンクリック!!")
+    },
+  },
+  components: { todo }
+}
+</script>
