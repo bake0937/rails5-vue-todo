f0cb7e7e6eae4951db07eb5c775e5884a22d22d8
bake0937 <sadayasu.info@gmail.com>
2017-07-31 01:59:13 +0900

    TODOステータスの変更を追加

--- app/javascript/packs/todo.vue
+++ app/javascript/packs/todo.vue
@@ -1,11 +1,39 @@
 <template>
   <li>
+    <span 
+        @click="complete(todo.id)" 
+        v-bind:class="{ completed: todo.completed }">
     {{ todo.title }}
+    </span>
   </li>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     props: ['todo'],
+    methods: {
+        complete: function(index) {
+          var todo_update = { 
+            "todo" : { 
+              "completed": !this.todo.completed 
+              } 
+          }
+          axios.put('/api/v1/todos/' + this.todo.id, todo_update)
+          .then(res => {
+              this.$parent.fetchTodos()
+            })
+            .catch(error => {
+              throw error
+            })
+        },
+    }
 }
 </script>
+
+<style scoped>
+.completed {
+  color: #ccc;
+  text-decoration:line-through;
+}
+</style>
